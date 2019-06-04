e36bdedcf63bcc7ccaded2ff9d006704e1190d46
Thiago Pereira Correa <thiago.correa147@hotmail.com>
2019-06-04 01:35:06 -0300

    First commit

---
+++ EPAgent.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.Generic;
+using ProjetoGrafos.DataStructure;
+
+namespace EP
+{
+    /// <summary>
+    /// EPAgent - searchs solution for the eight puzzle problem
+    /// </summary>
+    public class EightPuzzle : Graph
+    {
+        private int[] initState;
+        private int[] target;
+        private Graph solution = new Graph();
+
+        /// <summary>
+        /// Creating the agent and setting the initialstate plus target
+        /// </summary>
+        /// <param name="InitialState"></param>
+        public EightPuzzle(int[] InitialState, int[] Target)
+        {
+            initState = InitialState;
+            target = Target;
+            
+        }
+
+        /// <summary>
+        /// Accessor for the solution
+        /// </summary>
+        public int[] GetSolution()
+        {
+            return FindSolution();
+        }
+
+        /// <summary>
+        /// Função principal de busca
+        /// </summary>
+        /// <returns></returns>
+        private int[] FindSolution()
+        {
+            PriorityQueue pqueue = new PriorityQueue();
+            List<Node> temp = new List<Node>();
+            Node no = new Node("@",initState,0);
+            pqueue.Add(no);
+            while (!pqueue.IsEmpty())
+            {
+                no = pqueue.Pop();
+                if (TargetFound(no))
+                    return BuildAnswer(no);
+                temp = GetSucessors(no);
+                foreach(Node n in temp)
+                {
+                    pqueue.Add(n);
+                }
+            }
+            return null;
+        }
+
+        private List<Node> GetSucessors(Node n)
+        {
+            int [] estado = (int[])n.Info;
+            List<int> lista = new List<int>();
+            foreach (int i in estado)
+                lista.Add(i);
+            List<Node> nos = new List<Node>();
+            double tam = Math.Sqrt(lista.Count);
+            int index = lista.IndexOf(0), num = 0;
+
+            if ((index+1) <= tam)
+                num += 1;
+            if ((index+1) % tam == 0)
+                num += 2;
+            if ((index+1) > lista.Count - tam)
+                num += 4;
+            if ((index+1) == 1 || (index+1) % tam == 1)
+                num += 8;
+
+            switch(num)
+            {
+                case 0:
+                    if(checkState(n,index + 1))
+                        nos.Add(createState(n,index,index+1));
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, (index - Convert.ToInt32(tam))));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, (index + Convert.ToInt32(tam))));
+                    break;
+                case 1:
+                    if (checkState(n, index + 1))
+                        nos.Add(createState(n,index,index+1));
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index + Convert.ToInt32(tam)));
+                    break;
+                case 2:
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index - Convert.ToInt32(tam)));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index + Convert.ToInt32(tam)));
+                    break;
+                case 3:
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index + Convert.ToInt32(tam)));
+                    break;
+                case 4:
+                    if (checkState(n, index + 1))
+                        nos.Add(createState(n, index, index + 1));
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index - Convert.ToInt32(tam)));
+                    break;
+                case 6:
+                    if (checkState(n, index - 1))
+                        nos.Add(createState(n, index, index - 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index - Convert.ToInt32(tam)));
+                    break;
+                case 8:
+                    if (checkState(n, index + 1))
+                        nos.Add(createState(n, index, index + 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index - Convert.ToInt32(tam)));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index + Convert.ToInt32(tam)));
+                    break;
+                case 9:
+                    if (checkState(n, index + 1))
+                        nos.Add(createState(n, index, index + 1));
+                    if (checkState(n, index + Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index + Convert.ToInt32(tam)));
+                    break;
+                case 12:
+                    if (checkState(n, index + 1))
+                        nos.Add(createState(n, index, index + 1));
+                    if (checkState(n, index - Convert.ToInt32(tam)))
+                        nos.Add(createState(n, index, index - Convert.ToInt32(tam)));
+                    break;
+            }
+            return nos;
+
+        }
+
+        private bool checkState(Node n, int index)
+        {
+
+            if (n.parent == null)
+                return true;
+            else if (n.parent.Name == "@")
+            {
+                return true;
+            }
+            if (int.Parse(n.parent.Name) != ((int[])n.Info)[index])
+                return true;
+            return false;
+        }
+
+        private Node createState(Node n, int indexEmpty, int indexChange)
+        {          
+            int[] newState = new int[((int []) n.Info).Length];
+            for (int i = 0; i < ((int[])n.Info).Length; i++)
+            {
+                if (i == indexEmpty)
+                {
+                    newState[indexEmpty] = ((int[])n.Info)[indexChange];
+                    newState[indexChange] = ((int[])n.Info)[indexEmpty];
+                }
+                else if (i != indexChange)
+                {
+                    newState[i] = ((int[])n.Info)[i];
+                }
+            }
+            Node resp = new Node(((int[])n.Info)[indexChange].ToString(), newState, n.Nivel + 1);
+            n.Edges.Add(new Edge(n, resp));
+            resp.parent = n;
+            resp.grade = ManhattanDistance((int[])n.Info);
+            return resp;
+        }
+
+        private int[] BuildAnswer(Node n)
+        {
+            List<int> resp = new List<int>();
+            while(n.Name != "@")
+            {
+                resp.Add(int.Parse(n.Name));
+                n = n.parent;
+            }
+            resp.Reverse();
+            return resp.ToArray();
+        }
+
+        private bool TargetFound(Node n)
+        {
+            int count = 0;
+            foreach (int i in (int[])n.Info)
+            {
+                if(i != target[count])
+                    return false;
+                count++;
+            }
+            return true;
+        }
+
+        private int ManhattanDistance(int [] estado)
+        {
+            int tam = Convert.ToInt32(Math.Sqrt(estado.Length)), count = 0, distance = 0, x1, x2, y1, y2;
+            foreach(int a in estado)
+            {
+                if (a != 0)
+                {
+                    x1 = a / tam;
+                    x2 = count / tam;
+                    y1 = a % tam;
+                    y2 = count % tam;
+                    distance += Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+                    count++;
+
+                }
+            }
+            return distance;
+        }
+    }
+}
+
---
+++ PriorityQueue.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjetoGrafos.DataStructure;
+
+namespace EP
+{
+    public class PriorityQueue
+    {
+        List<Node> elements;
+
+        public PriorityQueue()
+        {
+            elements = new List<Node>();
+            
+        }
+        private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+        private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+        private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
+
+        private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < elements.Count;
+        private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < elements.Count;
+        private bool IsRoot(int elementIndex) => elementIndex == 0;
+
+        private int GetLeftChild(int elementIndex) => (elements[GetLeftChildIndex(elementIndex)].grade + elements[GetLeftChildIndex(elementIndex)].Nivel);
+        private int GetRightChild(int elementIndex) => (elements[GetRightChildIndex(elementIndex)].grade + elements[GetRightChildIndex(elementIndex)].Nivel);
+        private int GetParent(int elementIndex) => (elements[GetParentIndex(elementIndex)].grade + elements[GetParentIndex(elementIndex)].Nivel);
+
+        public bool IsEmpty()
+        {
+            return elements.Count == 0;
+        }
+        public Node Peek()
+        {
+            if (IsEmpty())
+                throw new IndexOutOfRangeException();
+
+            return elements[0];
+        }
+
+        public void Add(Node item)
+        {
+            elements.Add(item);
+            SortUp();
+        }
+
+        public void Delete(Node item)
+        {
+            int i = elements.IndexOf(item);
+            int last = elements.Count - 1;
+
+            elements[i] = elements[last];
+            elements.RemoveAt(last);
+            SortDown();
+        }
+
+        public Node Pop()
+        {
+            if (IsEmpty())
+                throw new IndexOutOfRangeException();
+            Node item = elements[0];
+            Delete(item);
+            return item;
+
+            //if (elements.Count > 0)
+            //{
+            //    T item = elements[0];
+            //    Delete(item);
+            //    return item;
+            //}
+            //relook at this - should we just throw exception?
+            //return default(T);
+        }
+
+        private void SortDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var smallerIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetRightChild(index) < GetLeftChild(index))
+                {
+                    smallerIndex = GetRightChildIndex(index);
+                }
+
+                if (SumGradeNivel(elements,smallerIndex) >= SumGradeNivel(elements,index))
+                {
+                    break;
+                }
+
+                SwapElements(smallerIndex, index);
+                index = smallerIndex;
+            }
+        }
+
+        private void SortUp()
+        {
+            var index = elements.Count - 1;
+            while (!IsRoot(index) && SumGradeNivel(elements,index) < GetParent(index))
+            {
+                var parentIndex = GetParentIndex(index);
+                SwapElements(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+
+        private int SumGradeNivel(List<Node> i, int index)
+        {
+            return i[index].Nivel + i[index].grade;
+        }
+
+        //private void Sort()
+        //{
+        //    for (int i = elements.Count - 1; i > 0; i--)
+        //    {
+        //        int parentPosition = (i + 1) / 2 - 1;
+        //        parentPosition = parentPosition >= 0 ? parentPosition : 0;
+
+        //        if (elements[parentPosition].CompareTo(elements[i]) > 1)
+        //        {
+        //            SwapElements(parentPosition, i);
+        //        }
+        //    }
+        //}
+
+        private void SwapElements(int firstIndex, int secondIndex)
+        {
+            Node tmp = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = tmp;
+        }
+    }
+}
